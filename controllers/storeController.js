3f49875b2d4c08522cdd6fcb2bb844c258a49f8b
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-06-05 16:49:39 +0200

    redirect to store after saving

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  req.flash('error', 'Something Happened');
-  req.flash('info', 'Something Happened');
-  req.flash('warning', 'Something Happened');
-  req.flash('success', 'Something Happened');
   res.render('index');
 };
 
@@ -14,11 +10,10 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
+  const store = await new Store(req.body).save();
   req.flash(
     'success',
     `Successfully Created ${store.name}. Care to leave a review?`
   );
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
 };
