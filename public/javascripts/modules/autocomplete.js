ad68de3516cd82eaf5f360652b1b7a4553e8770e
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-06-06 16:06:48 +0200

    console.log(place);

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,5 +1,11 @@
 function autocomplete(input, latInput, lngInput) {
-  console.log(input, latInput, lngInput);
+  if (!input) return; // skip this fn from running if there is not input on the page
+  const dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    console.log(place);
+  });
 }
 
 export default autocomplete;
