9aeb788e4f61641d5eec2aa6923fe7429c1ae49a
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-06-08 11:59:52 +0200

    validateRegister with validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,3 +7,34 @@ exports.loginForm = (req, res) => {
 exports.registerForm = (req, res) => {
   res.render('register', { title: 'Register' });
 };
+// see express-validator app.js => line 30
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'You must supply a name!').notEmpty();
+  req.checkBody('email', 'That Email is not valid!').isEmail();
+  // https://github.com/chriso/validator.js#sanitizers
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false,
+  });
+  req.checkBody('password', 'Password Cannot be blank!').notEmpty();
+  req
+    .checkBody('password-confirm', 'Confirmed Password cannot be blank!')
+    .notEmpty();
+  req
+    .checkBody('password-confirm', 'Oops! Your passwords do not match')
+    .equals(req.body.password);
+
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash('error', errors.map(err => err.msg));
+    res.render('register', {
+      title: 'Register',
+      body: req.body,
+      flashes: req.flash(),
+    });
+    return; // stop the fn from running
+  }
+  next(); // there were no errors!
+};
