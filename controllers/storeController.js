ef86c8ca505d3a8b0130db73dbd2fe78ea928d2e
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-06-10 14:29:20 +0200

    add author & link to user & populate

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -43,6 +43,7 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
+  req.body.author = req.user._id;
   const store = await new Store(req.body).save();
   req.flash(
     'success',
@@ -87,7 +88,9 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug });
+  const store = await Store.findOne({ slug: req.params.slug }).populate(
+    'author'
+  );
   if (!store) return next(); // app.js => 71 app.use('/', routes); next is 74 app.use(errorHandlers.notFound);
   res.render('store', { store, title: store.name });
 };
