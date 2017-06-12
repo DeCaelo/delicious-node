6c9bedba4c07cb607b5f491c60f570d92452a494
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-06-12 15:21:43 +0200

    hearts includes with $pull & $addToSet in User model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const User = mongoose.model('User');
 const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
@@ -156,4 +157,18 @@ exports.mapStores = async (req, res) => {
 
 exports.mapPage = (req, res) => {
   res.render('map', { title: 'Map' });
+};
+
+exports.heartStore = async (req, res) => {
+  const hearts = req.user.hearts.map(obj => obj.toString());
+  // $pull = mongodb operato to remove, $addToSet and not push because we want to be unique
+  const operator = hearts.includes(req.params.id) ? '$pull' : '$addToSet';
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      [operator]: { hearts: req.params.id },
+    },
+    { new: true }
+  );
+  res.json(user);
 };
