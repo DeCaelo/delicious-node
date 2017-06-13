139941997ccb1aa43e8c4d3faf85651621f1da43
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-06-13 16:03:42 +0200

    aggregation done

--- models/Store.js
+++ models/Store.js
@@ -94,9 +94,20 @@ storeSchema.statics.getTopStores = function() {
       },
     },
     // filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true } } },
     // Add the average reviews field
+    {
+      $project: {
+        photo: '$$ROOT.photo',
+        name: '$$ROOT.name',
+        reviews: '$$ROOT.reviews',
+        averageRating: { $avg: '$reviews.rating' },
+      },
+    },
     // sort it by our new field, highest reviews first
+    { $sort: { averageRating: -1 } },
     // limit to at most 10
+    { $limit: 10 },
   ]);
 };
 
